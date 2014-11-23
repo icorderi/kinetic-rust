90f70787c9f314fde2e1e55fdc692695ab554d0c
Travis <travis@travis-ci.org>
2014-11-23 08:22:43 +0000

    Travis build 27 pushed to gh-pages

--- doc/implementors/core/cmp/trait.Eq.js
+++ doc/implementors/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['kinetic'] = ["<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='kinetic/enum.KineticError.html' title='kinetic::KineticError'>KineticError</a>","<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='kinetic/enum.StatusCode.html' title='kinetic::StatusCode'>Command_Status_StatusCode</a>",];
+implementors['kinetic'] = ["<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='kinetic/error/enum.KineticError.html' title='kinetic::error::KineticError'>KineticError</a>","<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='kinetic/enum.StatusCode.html' title='kinetic::StatusCode'>Command_Status_StatusCode</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
