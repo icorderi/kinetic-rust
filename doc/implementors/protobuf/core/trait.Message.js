6988b182374289fa74ec762d5688d8ed7643ea27
Travis <travis@travis-ci.org>
2014-11-24 01:14:15 +0000

    Travis build 30 pushed to gh-pages

---
+++ doc/implementors/protobuf/core/trait.Message.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['kinetic'] = ["<a class='stability Experimental' title='Experimental'></a>impl Message for <a class='struct' href='kinetic/proto/struct.Message.html' title='kinetic::proto::Message'>Message</a>","<a class='stability Experimental' title='Experimental'></a>impl Message for <a class='struct' href='kinetic/proto/message/struct.HmacAuth.html' title='kinetic::proto::message::HmacAuth'>Message_HMACauth</a>","<a class='stability Experimental' title='Experimental'></a>impl Message for <a class='struct' href='kinetic/proto/message/struct.PinAuth.html' title='kinetic::proto::message::PinAuth'>Message_PINauth</a>","<a class='stability Experimental' title='Experimental'></a>impl Message for <a class='struct' href='kinetic/proto/struct.Command.html' title='kinetic::proto::Command'>Command</a>","<a class='stability Experimental' title='Experimental'></a>impl Message for <a class='struct' href='kinetic/proto/command/struct.Header.html' title='kinetic::proto::command::Header'>Command_Header</a>","<a class='stability Experimental' title='Experimental'></a>impl Message for <a class='struct' href='kinetic/proto/command/struct.Body.html' title='kinetic::proto::command::Body'>Command_Body</a>","<a class='stability Experimental' title='Experimental'></a>impl Message for <a class='struct' href='kinetic/proto/command/struct.Status.html' title='kinetic::proto::command::Status'>Command_Status</a>","<a class='stability Experimental' title='Experimental'></a>impl Message for <a class='struct' href='kinetic/proto/command/struct.KeyValue.html' title='kinetic::proto::command::KeyValue'>Command_KeyValue</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
