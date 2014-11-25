ac3f0e095e6f0a4046adebb8fae734ca0861fe44
Travis <travis@travis-ci.org>
2014-11-25 20:17:32 +0000

    Travis build 36 pushed to gh-pages

---
+++ doc/implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['kinetic'] = ["<a class='stability Unstable' title='Unstable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='kinetic/channel/struct.KineticChannel.html' title='kinetic::channel::KineticChannel'>KineticChannel</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
