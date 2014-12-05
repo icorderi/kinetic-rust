300d01b6af2f31815eb22595dae50dc21d6b7331
Travis <travis@travis-ci.org>
2014-12-05 02:51:51 +0000

    Travis build 56 pushed to gh-pages

---
+++ doc/implementors/std/io/trait.Writer.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['kinetic-rust'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Writer.html' title='std::io::Writer'>Writer</a> for <a class='struct' href='kinetic-rust/shell/struct.Shell.html' title='kinetic-rust::shell::Shell'>Shell</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
